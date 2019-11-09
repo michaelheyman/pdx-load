812e57359761661f6a0bc8013172c61f24da3b66
michaelheyman <mitch.heyman@gmail.com>
2019-11-09 07:06:07 -0800

    Add SQLAlchemy ORM

---
+++ app/database/__init__.py
@@ -0,0 +1,7 @@
+from .model import ClassOfferingMgr
+from .model import CourseMgr
+from .model import initialize_database
+from .model import InstructorMgr
+from .model import TermMgr
+
+__all__ = [ClassOfferingMgr, CourseMgr, initialize_database(), InstructorMgr, TermMgr]
