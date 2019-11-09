15ff3a8a8d921a3217dcd3a484dbaf196441b0a4
michaelheyman <mitch.heyman@gmail.com>
2019-11-09 07:25:39 -0800

    Create SQLite database from metadata

--- app/config.py
+++ app/config.py
@@ -15,5 +15,8 @@ def map_level(level):
     )
 
 
+DATABASE_PATH = os.environ.get("DATABASE_PATH", "app.db")
 LOGGING_LEVEL = map_level(os.environ.get("LOGGING_LEVEL", "debug"))
+MAX_TERMS = int(os.environ.get("MAX_TERMS", "1"))
+PROJECT_DIR = os.path.abspath(os.curdir)
 PROCESSED_BUCKET_NAME = os.environ.get("BUCKET_NAME", "pdx-schedule-processed-data")
--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 import json
+import os
 
 from google.cloud import storage
 
 from app import config
+from app.database import ClassOfferingMgr
+from app.database import CourseMgr
+from app.database import initialize_database
+from app.database import InstructorMgr
+from app.database import TermMgr
 from app.logger import logger
 
 
@@ -27,12 +33,105 @@ def get_latest_blob():
     return latest_blob
 
 
-def run():
-    latest_blob = get_latest_blob()
-    contents = latest_blob.download_as_string()
+def load_local_file():
+    local_file = "snapshot.json"
+    local_filename = os.path.join(config.PROJECT_DIR, local_file)
+    res = None
+    with open(local_filename) as infile:
+        res = json.load(infile)
+
+    return res
+
+
+def get_unique_instructors(instructor_list):
+    instructors = []
+    for inst in instructor_list:
+        instructors.append(inst)
+
+    return filter_instructor_name(instructors)
+
+
+def filter_instructor_name(instructors):
+    return list({v["fullName"]: v for v in instructors}.values())
+
+
+def extract_metadata(contents):
+    """Extracts course and instructor information from the bucket metadata
+
+    :param contents: Bucket metadata
+    :return: list of terms, courses, and instructors
+    """
+
+    terms = []
+    instructors = []
+    courses = []
+    for term in contents:
+        for course in term:
+            courses.append(course)
+            instructors.append(course["instructor"])
+
+    return terms, courses, instructors
+
+
+def write_to_database(contents):
+    terms, courses, instructors = extract_metadata(contents)
+
+
+def save_to_database(course):
+    if course["instructor"] is None:  # NOTE: are there ever NO instructors?
+        CourseMgr.add_course(
+            name=course["name"],
+            number=course["number"],
+            discipline=course["discipline"],
+        )
+        return
+
+    InstructorMgr.add_instructor(course["instructor"])
+    CourseMgr.add_course(
+        name=course["name"], number=course["number"], discipline=course["discipline"]
+    )
+
+    ClassOfferingMgr.add_class_offering(
+        course_name=course["name"],
+        course_number=course["number"],
+        # instructor_name=course["instructor"],
+        # TODO: how can we get a default value for this access,
+        #  like course.get(["instructor"]["fullName"], None)?
+        instructor_name=course["instructor"]["fullName"]
+        if course["instructor"]
+        else None,
+        term=course["term_date"],
+        credits=course["credits"],
+        days=course["days"],
+        time=course["time"],
+        crn=course["crn"],
+    )
+
+    TermMgr.add_term(date=course["term_date"], description=course["term_description"])
+
+
+def cleanup_lambda_files():
+    # this won't be necessary in the lambda because it won't save state
     try:
-        contents_json = json.loads(contents)
-    except json.decoder.JSONDecodeError as e:
-        logger.critical(f"Error decoding JSON: {e}")
-        exit()
-    return contents_json
+        os.remove(config.DATABASE_PATH)
+    except FileNotFoundError:
+        pass
+
+
+def run():
+    contents = load_local_file()  # TODO: remove after testing
+    cleanup_lambda_files()
+    initialize_database()
+    for term in contents:
+        for course in term:
+            save_to_database(course)
+    # write_to_database(contents)
+
+    # latest_blob = get_latest_blob()
+    # contents = latest_blob.download_as_string()
+    # try:
+    #     contents_json = json.loads(contents)
+    # except json.decoder.JSONDecodeError as e:
+    #     logger.critical(f"Error decoding JSON: {e}")
+    #     exit()
+    # return contents_json
